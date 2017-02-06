405c6b85b8b962f36cfb9a700a4cea18f1186557
Ahsan Feroz <a.m.ahsan.feroz@gmail.com>
2017-02-06 14:47:56 +0200

    Data shown in logcat from apiURL (url test)

--- app/src/main/java/arora/ahsanferoz/stormy/MainActivity.java
+++ app/src/main/java/arora/ahsanferoz/stormy/MainActivity.java
@@ -1,13 +1,56 @@
 package arora.ahsanferoz.stormy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "dbbd4717c2097d41eb83b7738b570a35";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+        String forcastURL = "https://api.darksky.net/forecast/" + apiKey +
+                "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(forcastURL)
+                .build();
+
+        okhttp3.Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+            }
+        });
+
+
     }
 }
